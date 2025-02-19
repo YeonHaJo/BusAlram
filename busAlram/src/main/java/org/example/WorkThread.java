33ba2201415ae5dc5427da645ffc8add83bf0bd7
[YeonHaJo] <[yeonhajo65@gmail.com]>
2025-02-19 13:51:26 +0900

    브랜치2 커밋

--- busAlram/src/main/java/org/example/WorkThread.java
+++ busAlram/src/main/java/org/example/WorkThread.java
@@ -38,6 +38,7 @@ public class WorkThread extends Thread {
             System.out.println("wake up !!!!!!");
             System.out.println("new branch create test 2st");
             System.out.println("new branch create test 3st");
+            System.out.println("new branch create test 4st");
         }catch (InterruptedException  e){
             e.getMessage();
         }
